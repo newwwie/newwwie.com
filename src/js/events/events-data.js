16e2f6bfc0ff0a2591b59715b28efd165b7d2c7d
Christopher Baita <75826057+clbaita@users.noreply.github.com>
2022-05-07 16:35:14 +1000

    updated all packages (#57)

--- src/js/events/events-data.js
+++ src/js/events/events-data.js
@@ -1,4 +1,4 @@
-// Auto Generated on 2022-05-06T13:38:12.706Z
+// Auto Generated on 2022-05-07T03:24:44.048Z
  module.exports = [
   {
     "event": {
@@ -72,7 +72,7 @@
       "description": "Hello friends! Welcome back to another instalment of NCSG.\n\nThis month, we're lucky to have Dr. Alana Maurushat, Director of CACE (the Western Centre for Cybersecurity Aid & Community Engagement). Dr. Maurushat will be talking about the world of cybercrime and its evolution, and considering her background, this'll be an interesting dive into the underbelly of cybersecurity.\n\nTo learn more about Dr. Maurushat, check out her LinkedIn [here](https://www.linkedin.com/in/alana-maurushat-587116204/?originalSubdomain=au), as well as her profile on AustCyber [here](https://www.austcyber.com/alana-maurushat).\n\n**Event details:**\n\nDate: June 2, 2022 (aka May 33rd 😎)\nTime: 18:00-20:00ish (or until we go to the pub)\nVenue: 16 Honeysuckle Drive, Newcastle (The University of Newcastle 'Q Building') - seminar space on Level 2\nFood: Provided, please inform us of dietary requirements when RSVPing\nDrink: Non-alcoholic provided, come and grab a few with us at the pub after if you'd like!\n\n**The rest:**\n\nIf you’re experiencing any symptoms representative of COVID-19, please don’t attend. We’ll miss you, but the health/safety of attendees is paramount.\n\nNCSG is a community group determined to improve and uplift the Newcastle cybersecurity community. We meet monthly (on the last Thursday of each month... except for this one) and have a bunch of interesting speakers, from local legends to heavy hitters in the enterprise world.\n\nWant to learn more? Be sure to follow us on LinkedIn: linkedin.com/company/newcastlecybersecuritygroup/ or visit newwwie.com, join the community and come and chat in the #security channel.",
       "dateTime": "2022-06-02T18:00+10:00",
       "eventUrl": "https://www.meetup.com/Newcastle-Cyber-Security-Group/events/285731908",
-      "going": 10,
+      "going": 11,
       "maxTickets": 50,
       "duration": "PT2H",
       "imageUrl": "https://secure-content.meetupstatic.com/images/classic-events/503902798/676x380.webp",
