86e26d154a5e8863f1c3fd10f21ff90f38f3b2dd
Christopher Baita <75826057+clbaita@users.noreply.github.com>
2022-07-29 11:02:56 +1000

    Replace depreciated node-sass dependency with sass

--- src/js/events/events-data.js
+++ src/js/events/events-data.js
@@ -1,78 +1,12 @@
-// Auto Generated on 2022-07-26T13:45:23.833Z
+// Auto Generated on 2022-07-28T23:54:50.914Z
  module.exports = [
-  {
-    "event": {
-      "title": "July Meeting: Cole Cornford on JavaScript Security",
-      "description": "This month, we are most fortunate to have **Cole Cornford** speaking to us about JavaScript security.\n\n**Security concerns for the Web's favourite language!**\n\nJavaScript is now the most popular client and server-side language in use in 2022. It's a big target for adversaries, and there are a tremendous amount of ways that they work to get code execution happening in your app.\n\nJoin Cole to learn about the techniques adversaries use to gain script injection, what you can do about those, and what you can do to build safer web applications.\n\nCole Cornford is the Director of Galah Cyber, an Australian Cybersecurity consultancy with a focus on Application, Cloud, and Product Security. Cole has worked in the security space for 8 years across a variety of industries and cybersecurity fields. Cole helps organise a few other Newcastle communities, as well as being a regular presenter at AISA, BSides, and OWASP Conferences.\n\nDate: Wednesday 27th of July\nLocation: Mudbath\nUnit 8 (Upstairs) 710 Hunter Street. Entry via the left side of the building.\nLivestream: https://livestream.mudbath.com.au/\n\nOff-street parking is available directly behind the offices, entry is via the ibis hotel driveway.\n\nThanks to the Newy Tech People for sponsoring pizza.\n---\n\nDon't be insecure, come along!",
-      "dateTime": "2022-07-27T18:00+10:00",
-      "eventUrl": "https://www.meetup.com/newcastlejs-javascript-meetup/events/287320127",
-      "going": 19,
-      "maxTickets": 50,
-      "duration": "PT3H",
-      "imageUrl": "https://secure-content.meetupstatic.com/images/classic-events/473023676/676x380.webp",
-      "venue": {
-        "name": "710 Hunter St",
-        "lat": -32.926094,
-        "lng": 151.76259,
-        "address": "710 Hunter St",
-        "city": "Newcastle West"
-      }
-    },
-    "group": {
-      "name": "NewcastleJS - JavaScript Meetup",
-      "urlname": "newcastlejs-javascript-meetup",
-      "groupPhoto": {
-        "id": "473023676",
-        "baseUrl": "https://secure-content.meetupstatic.com/images/classic-events/",
-        "preview": null
-      },
-      "logo": {
-        "id": "473023676",
-        "baseUrl": "https://secure-content.meetupstatic.com/images/classic-events/",
-        "preview": null
-      }
-    }
-  },
-  {
-    "event": {
-      "title": "NCSG, July 2022 | Andrew Stace",
-      "description": "Hello friends! Welcome back to another instalment of NCSG.\n\nThis month, we're lucky to have Andrew Stace, the fearless leader and founder of our lovely group. He currently works as a cybersecurity professional for an ASX-listed company, and the grand finale of his presentation will be him 100%ing a variety of songs on Beat Saber.\n\nSynopsis of Andrew's talk:\n\nCybersecurity scams are increasing in frequency & impact. Meaning that more people than ever before are coming to be impacted by some form of cyber crime.\n\nBut often our attention is focused on the impact being described in financial or technical framings. Instead, in this talk, Andrew will explore some of the emotional impact of cybercrime &/or the emotional context(s) in which it can prosper.\n\n**Event details:**\n\nDate: July 28th, 2022\nTime: 18:00-20:00ish (or until we go to the pub)\nVenue: 16 Honeysuckle Drive, Newcastle (The University of Newcastle 'Q Building') - second floor 'seminar space'\nFood: Provided, please inform us of dietary requirements when RSVPing\nDrink: Non-alcoholic provided, come and grab a few with us at the pub after if you'd like!\n\n**The rest:**\n\nIf you’re experiencing any symptoms representative of COVID-19, please don’t attend. We’ll miss you, but the health/safety of attendees is paramount.\n\nNCSG is a community group determined to improve and uplift the Newcastle cybersecurity community. We meet monthly (on the last Thursday of each month) and have a bunch of interesting speakers, from local legends to heavy hitters in the enterprise world.\n\nWant to learn more? Be sure to follow us on LinkedIn: linkedin.com/company/newcastlecybersecuritygroup/ or visit newwwie.com, join the community and come and chat in the #security channel.",
-      "dateTime": "2022-07-28T18:00+10:00",
-      "eventUrl": "https://www.meetup.com/newcastle-cyber-security-group/events/286991107",
-      "going": 25,
-      "maxTickets": 50,
-      "duration": "PT2H",
-      "imageUrl": "https://secure-content.meetupstatic.com/images/classic-events/505335833/676x380.webp",
-      "venue": {
-        "name": "Q Building, The University of Newcastle",
-        "lat": -32.926277,
-        "lng": 151.7683,
-        "address": "16B Honeysuckle Dr",
-        "city": "Newcastle"
-      }
-    },
-    "group": {
-      "name": "Newcastle Cyber Security Group",
-      "urlname": "Newcastle-Cyber-Security-Group",
-      "groupPhoto": {
-        "id": "483205216",
-        "baseUrl": "https://secure-content.meetupstatic.com/images/classic-events/",
-        "preview": null
-      },
-      "logo": {
-        "id": "483205216",
-        "baseUrl": "https://secure-content.meetupstatic.com/images/classic-events/",
-        "preview": null
-      }
-    }
-  },
   {
     "event": {
       "title": "August: Containers are confusing!  A hands on look at containers and Kubernetes.",
       "description": "For our August meeting we'll be talking about containers and orchestrating them with Kuberneties.\n\nWe'll be hosted by MudBath with sponsorship from Saphi.\n\n**The Talk:** Containers are confusing and there are a few misconceptions about what containers are. In this presentation Alister will demonstrate what containers are, what they aren't and how to reason about them. Once you understand what containers are, you'll have a better time building and running them. Bring along your laptop with docker and Kubernetes installed and you can try out the commands yourself on the night!\n\n**About Alistair:** I'm a senior software developer working at the Greater Bank where I primarily work on backend services, using Kotlin, deployed on Kubernetes. I've been doing software for over 25 years, so these days i'm \"old man who yells at clouds\"\n\n**Getting to the event:**\nMubath is located in 710 Hunter street in the 2nd floor of the building.\nThere is some on street parking available.\nThe closest public transport is trains from the Newcastle Interchange, there is a bus stop at the venue.",
       "dateTime": "2022-08-03T18:00+10:00",
       "eventUrl": "https://www.meetup.com/newcastle-coders-group/events/286934350",
-      "going": 15,
+      "going": 18,
       "maxTickets": 0,
       "duration": "PT3H",
       "imageUrl": "https://secure-content.meetupstatic.com/images/classic-events/505406540/676x380.webp",
@@ -105,7 +39,7 @@
       "description": "This month, hear from fellow Novocastrian Tim Snell, Director at IMC and Founder at PitCrew AI, as he leads us through his journey in business and developing remote computer vision applications using thermal and non-visible light imaging cameras for some of the biggest names in industry.\n\n**About the speaker:** Tim Snell is the Managing Director of Industrial Monitoring & Control Pty Ltd, including the Pitcrew AI business unit. Based in Newcastle, Australia, Tim has a bachelor’s degree in engineering (computer), an MBA and extensive technical and business management experience. Tim has a track record of developing and commercialising innovative computer vision technologies.\n\n**Networking**: As always, there will be plenty of time for questions and networking!",
       "dateTime": "2022-08-04T18:30+10:00",
       "eventUrl": "https://www.meetup.com/newcastle-iot-pioneers/events/287415669",
-      "going": 11,
+      "going": 12,
       "maxTickets": 0,
       "duration": "PT2H",
       "imageUrl": "https://secure-content.meetupstatic.com/images/classic-events/505818221/676x380.webp",
@@ -134,11 +68,44 @@
   },
   {
     "event": {
+      "title": "Newy WGDPTech Casual Catch-up",
+      "description": "Hello there!\n\nWe're hosting another event for the Newcastle Women and Gender People in Tech meeting, and we'd love for you to join us!\n\nThis event will take the form of a casual networking and meetup. It's a fantastic opportunity to meet others in the space and build a community, as well as provide input in next months speaker event.\n\nIf you're in tech, interested in tech or just want to come along to see what we're about, you're more than welcome, we'd love to have you!\n\n**The details:**\n\n* **When**: Thursday, 11th August from 5:30PM to 7:00PM\n* **Where:** Honeysuckle Hotel\n\nWe hope to see you there :)",
+      "dateTime": "2022-08-11T17:30+10:00",
+      "eventUrl": "https://www.meetup.com/newcastle-women-and-gender-diverse-people-in-tech/events/287438490",
+      "going": 4,
+      "maxTickets": 0,
+      "duration": "PT1H30M",
+      "imageUrl": "https://secure-content.meetupstatic.com/images/classic-events/504541880/676x380.webp",
+      "venue": {
+        "name": "Honeysuckle Hotel",
+        "lat": -32.92507,
+        "lng": 151.76918,
+        "address": "",
+        "city": "Newcastle"
+      }
+    },
+    "group": {
+      "name": "Newcastle Women and Gender Diverse People in Tech",
+      "urlname": "newcastle-women-and-gender-diverse-people-in-tech",
+      "groupPhoto": {
+        "id": "504544595",
+        "baseUrl": "https://secure-content.meetupstatic.com/images/classic-events/",
+        "preview": null
+      },
+      "logo": {
+        "id": "504544595",
+        "baseUrl": "https://secure-content.meetupstatic.com/images/classic-events/",
+        "preview": null
+      }
+    }
+  },
+  {
+    "event": {
       "title": "data.AUG <- Stats, ML and ops: presented by Team HH",
       "description": "This month, there's something for everyone with our Trio of Analytics Dips 😋, courtesy of Team Honeysuckle Health:\n\n* **Stats**: Imagine you organised a great big initiative, and it just concluded yesterday. That initiative could be your marketing campaign, staff training course, health intervention, etc.\nYou feel like things went well, but you wish you'd thought about *proving* the success of your initiative with *hard numbers*. But it's all too late, now that the intervention is complete... right?\nFear not! We'll talk about observational studies and look at how they can quantify the success of your intervention, even *after the fact*. ⚖\n* **Machine Learning**: Sometimes you need to compare things that you can't quite measure directly 📏.\nTake, for instance, the cost of the *same* surgery. Though the same procedure was delivered, the price will still depend on the patient's age, where they live, their medical history and risks. How can we tell if a surgery was overpriced... when no two are different? Answer in this talk: it involves ML 🧠\n* **Ops**: Making all of this come together, at scale, for real-time insights.\n\nVENUE:\nThe university's brand new Q building in Honeysuckle!\n\nhttps://goo.gl/maps/35LwcLhKV1nvoFAh7\n\nIMPORTANT this is NOT the NewSpace building where we met last year.\n\nWe will meet in the Level 2 Seminar Space. You'll need to enter through the main doors on Worth Place and will be directed to the elevators to join the meetup.\n\nPARKING: The closest parking is Wright Lane Carpark.\nhttps://goo.gl/maps/rj6Yg8EkpjYzUmbi7\n\nAfter 5pm it is free and is a 400m - 5 minute walk.\n\n##-------------------------------------------------------------------##\n\nTIME: 5.30pm - 7.30pm\n\nFOOD: 🍕 + 🥤\n\n* Wood fired pizza, sponsored by Newy Tech People\n* Please advise dietary requirements in RSVP\n* Soft drinks\n\nSCHEDULE:\n\n* 5:30pm: Networking + Food + Drinks\n(Don't stress if you are held up in traffic)\n* 6:15pm: Presentation\n* 7:15pm: Packing up + Bonus questions\n* 7:30pm: Close",
       "dateTime": "2022-08-18T17:30+10:00",
       "eventUrl": "https://www.meetup.com/hunter-data-analytics/events/283702285",
-      "going": 8,
+      "going": 9,
       "maxTickets": 40,
       "duration": "PT2H",
       "imageUrl": "https://secure-content.meetupstatic.com/images/classic-events/501789003/676x380.webp",
