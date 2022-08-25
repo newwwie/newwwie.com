3c7b4356c11166d749040cb5055cca83d595f996
Josh Peak <neozenith.dev@gmail.com>
2022-08-25 10:30:06 +1000

    update reference to 90 day horizon filter

--- fetch-events/index.js
+++ fetch-events/index.js
@@ -39,7 +39,7 @@ Promise.all(
     return getGroupEvents(event).then(transformEvents);
   })
 ).then((events) => {
-  const sortedEvents = _.sortBy(events.flat().filter(filterEventsNext30Days), (e) => e.event.dateTime);
+  const sortedEvents = _.sortBy(events.flat().filter(filterEventsNext90Days), (e) => e.event.dateTime);
   fs.writeFileSync(
     path.join(__dirname, "..", "src", "js", "events", "events-data.js"),
     `// Auto Generated on ${new Date().toISOString()}\n module.exports = ${JSON.stringify(sortedEvents, null, 2)}`
