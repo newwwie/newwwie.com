cdda54c31b93e5dfa5f9a6af1efb6e4d354d6dff
Aaron Freemantle <afreemantle@camplify.com.au>
2024-03-17 22:09:08 +1100

    Format all the things

--- src/js/communities/Communities.js
+++ src/js/communities/Communities.js
@@ -1,13 +1,10 @@
-import {h, render} from 'preact';
-import { CommunityCard } from './CommunityCard';
-import { CommunityData } from './community-data';
+import { h, render } from "preact";
+import { CommunityCard } from "./CommunityCard";
+import { CommunityData } from "./community-data";
 
-export const Communities = ()=>{
-    return(
-    CommunityData.map((val)=>{
-        return <CommunityCard data={val}/>
-    }))
-
-
-}
-render(<Communities/>, document.getElementById("groups"))
+export const Communities = () => {
+  return CommunityData.map((val) => {
+    return <CommunityCard data={val} />;
+  });
+};
+render(<Communities />, document.getElementById("groups"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,54 @@
-'use strict';
+"use strict";
 
-const path                    = require('path');
-const autoprefixer            = require("autoprefixer");
-const MiniCssExtractPlugin    = require("mini-css-extract-plugin");
-const CssMinimizerPlugin      = require("css-minimizer-webpack-plugin");
-const TerserPlugin            = require('terser-webpack-plugin');
-
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    mode: "production",
-    entry: ['./src/js/main.js', './src/scss/main.scss'],
-    optimization: {
-        minimizer: [
-          new CssMinimizerPlugin(),
-        ]
+  mode: "production",
+  entry: ["./src/js/main.js", "./src/scss/main.scss"],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+  output: {
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
       },
-    output: {
-        filename: 'js/[name].js',
-        path: path.resolve(__dirname, 'dist/'),
-        publicPath: "/dist",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [ 
-                    MiniCssExtractPlugin.loader, 
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader' 
-                ]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css',
-        }),
-        (compiler) => {
-            new TerserPlugin({
-                terserOptions: {
-                    compress: {},
-                }
-            }).apply(compiler);
-        },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
     ],
-    // @see https://webpack.js.org/configuration/dev-server/ 
-    devServer: {
-        static: {
-            directory: __dirname
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
+    (compiler) => {
+      new TerserPlugin({
+        terserOptions: {
+          compress: {},
         },
-        compress: true,
-        host: '0.0.0.0',
-        port: 9000
-    }
+      }).apply(compiler);
+    },
+  ],
+  // @see https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    static: {
+      directory: __dirname,
+    },
+    compress: true,
+    host: "0.0.0.0",
+    port: 9000,
+  },
 };
