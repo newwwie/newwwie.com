9983d0c273d56cd066ec2aaa1078e4ea56b5fc7f
jayrovacsek <jay@rovacsek.com>
2024-10-28 23:55:46 +1100

    feat: sanitise dom elements where injected dangerously

--- src/js/events/Meetup.tsx
+++ src/js/events/Meetup.tsx
@@ -2,6 +2,8 @@ import { ComponentChildren, render } from "preact";
 import { events } from "./events-data";
 import { EventItem, Group, Image, Venue } from "./types";
 import showdown from "showdown"
+import DOMPurify from 'dompurify';
+
 export class Meetup {
   /**
    * @name meetupResponse
@@ -69,7 +71,9 @@ export class Meetup {
               Hosted by: <a href={`https://meetup.com/${group.urlname}`}>{group.name}</a>
             </p>
 
-            <p class="eventItem-description" dangerouslySetInnerHTML={{ __html: `${formattedEventDescription}...` }} />
+            <p class="eventItem-description"
+            dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(formattedEventDescription) }} 
+            />
             <ul class="eventItem-stats">
               <li class="eventItem-stats_rsvp" title="Number of people who have RSVP'd vs. the total number of spots">
                 <svg
