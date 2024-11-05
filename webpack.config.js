eda13be995ee267c37df7ae1bd54089595fb4c56
Josh Peak <joshpeak05@gmail.com>
2024-11-05 14:30:26 +1100

    fix: Address changes in Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,27 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 
 module.exports = {
   mode: "production",
   entry: ["./src/js/main.ts", "./src/scss/main.scss"],
   optimization: {
-    minimizer: [new CssMinimizerPlugin()],
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: { drop_console: true },
+        },
+      }),
+      new CssMinimizerPlugin(),
+    ],
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 244000, // Adjust this limit based on your target size
+    },
   },
   output: {
     filename: "js/[name].js",
@@ -42,6 +57,7 @@ module.exports = {
     extensions: [".wasm", ".ts", ".tsx", ".mjs", ".cjs", ".js", ".json", ".scss", ".css"],
   },
   plugins: [
+    // new BundleAnalyzerPlugin(),
     new MiniCssExtractPlugin({
       filename: "css/[name].css",
     }),
