5a408685cfef25492cb6d18cc8b32e40d3d3fa1a
jayrovacsek <jay@rovacsek.com>
2024-11-05 14:22:04 +1100

    feat: make meetup component adherent

--- src/js/events/types.ts
+++ src/js/events/types.ts
@@ -2,39 +2,39 @@
 // the graphql schema documented at https://www.meetup.com/api/schema
 
 export type EventItem = {
-  readonly event: Event;
-  readonly group: Group;
+  event: Event;
+  group: Group;
 };
 
 export type Event = {
-  readonly dateTime: null | string;
-  readonly description: null | string;
-  readonly duration: string;
-  readonly eventUrl: string;
-  readonly going: number;
-  readonly imageUrl: string;
-  readonly maxTickets: number;
-  readonly title: null | string;
-  readonly venue: null | Venue;
+  dateTime: string;
+  description: string;
+  duration: string;
+  eventUrl: string;
+  going: number;
+  imageUrl: string;
+  maxTickets: number;
+  title: string;
+  venue: Venue | null;
 };
 
 export type Venue = {
-  readonly address: null | string;
-  readonly city: null | string;
-  readonly lat: number;
-  readonly lng: number;
-  readonly name: null | string;
+  address: null | string;
+  city: null | string;
+  lat: number;
+  lng: number;
+  name: null | string;
 };
 
 export type Group = {
-  readonly groupPhoto: null | Image;
-  readonly logo: null | Image;
-  readonly name: null | string;
-  readonly urlname: null | string;
+  groupPhoto: null | Image;
+  logo: null | Image;
+  name: string;
+  urlname: string;
 };
 
 export type Image = {
-  readonly baseUrl: string;
-  readonly id: string;
-  readonly preview: null;
+  baseUrl: string;
+  id: string;
+  preview: null;
 };
